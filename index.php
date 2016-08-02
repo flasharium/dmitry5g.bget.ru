cac22dca621a31ae0b7b2dee39762777c414bb94
Dmitry Ivashenko <d.ivashenko@corp.mail.ru>
2016-08-02 22:13:09 +0300

    test

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <?php
-
+ini_set('display_errors', true);
 error_reporting(E_ALL);
 
-set_include_path(get_include_path() . PATH_SEPARATOR . './google-api-php-client/src');
+set_include_path(get_include_path() . PATH_SEPARATOR . 'libs/google-api-php-client/src');
 
+require_once dirname(__FILE__) . '/libs/google-api-php-client/src/Google/autoload.php';
 
 $client = new Google_Client();
-$client->setAuthConfigFile('config.json');
+$client->setClientId("783339917195-vb3h61cp2k1neqnsepej0u95gb4ta9ip.apps.googleusercontent.com");
+$client->setClientSecret("F4Jxi8O_IH-i7LkmnL4AjD4B");
+$client->setRedirectUri('http://' . $_SERVER['HTTP_HOST']);
+//$client->setAuthConfigFile('config.json');
 $client->addScope(Google_Service_Drive::DRIVE_METADATA_READONLY);
 $client->setRedirectUri('http://' . $_SERVER['HTTP_HOST'] . '/index.php');
 
