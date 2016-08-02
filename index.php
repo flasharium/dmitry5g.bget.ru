c46c6f792d73e8bad70c0be2c583e9e207c6fe83
Dmitry Ivashenko <d.ivashenko@corp.mail.ru>
2016-08-02 22:41:34 +0300

    test

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 ini_set('display_errors', true);
 error_reporting(E_ALL);
 
@@ -7,12 +8,11 @@ set_include_path(get_include_path() . PATH_SEPARATOR . 'libs/google-api-php-clie
 require_once dirname(__FILE__) . '/libs/google-api-php-client/src/Google/autoload.php';
 
 $client = new Google_Client();
-$client->setClientId("783339917195-vb3h61cp2k1neqnsepej0u95gb4ta9ip.apps.googleusercontent.com");
-$client->setClientSecret("F4Jxi8O_IH-i7LkmnL4AjD4B");
+$client->setClientId("783339917195-31t7vm567pgvrat30lt0p72uf4lmkdcq.apps.googleusercontent.com");
+$client->setClientSecret("67DeEScrD5RPhJsJDXyhoB09");
 $client->setRedirectUri('http://' . $_SERVER['HTTP_HOST']);
-//$client->setAuthConfigFile('config.json');
-$client->addScope(Google_Service_Drive::DRIVE_METADATA_READONLY);
-$client->setRedirectUri('http://' . $_SERVER['HTTP_HOST'] . '/index.php');
+$client->addScope(Google_Service_AdSense::ADSENSE_READONLY);
+$client->setRedirectUri('http://dmitry5g.bget.ru/');
 
 if (isset($_REQUEST['logout'])) {
     unset($_SESSION['access_token']);
@@ -21,20 +21,28 @@ if (isset($_REQUEST['logout'])) {
 if (isset($_GET['code'])) {
     $client->authenticate($_GET['code']);
     $_SESSION['access_token'] = $client->getAccessToken();
-    $redirect = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
+//    var_dump($client->getAccessToken());
+    $redirect = 'http://dmitry5g.bget.ru/';
     header('Location: ' . filter_var($redirect, FILTER_SANITIZE_URL));
 }
 
 if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
     $client->setAccessToken($_SESSION['access_token']);
-} else {
+}
+else {
     $authUrl = $client->createAuthUrl();
 }
 
 if ($client->getAccessToken()) {
     $adsenseService = new Google_Service_AdSense($client);
-    $resource = $adsenseService->reports->generate("today-1m", "today");
-    var_dump($resource);
+    $resource = $adsenseService->reports->generate("today-1d", "today");
+    var_dump($resource->getTotals());
 
     $_SESSION['access_token'] = $client->getAccessToken();
 }
+
+if (isset($authUrl)) {
+    echo "<a class='login' href='" . $authUrl . "'>Connect Me!</a>";
+}
+
+echo "end";
